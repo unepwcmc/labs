e0e4ef5ee3226766f45feede4ebb41bb129adeac
Simao Belchior de Castro <simaobelchior@gmail.com>
2011-12-10 10:28:20 +0000

    trying to fix asset compilation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,4 +57,5 @@ Labs::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+  config.assets.js_compressor = :uglifier
 end
