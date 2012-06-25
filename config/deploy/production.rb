dbeaf57dc3a72b977766f5b1a53439d334fb9e7f
Adam Mulligan <adammulligan@gmail.com>
2012-06-25 11:20:10 +0100

    Fixed silly logic error in index template

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,5 +1,7 @@
 # Primary domain name of your application. Used in the Apache configs
 set :domain, "unepwcmc-004.vm.brightbox.net"
 
+set :rake, 'bundle exec rake'
+
 ## List of servers
 server "unepwcmc-004.vm.brightbox.net", :app, :web, :db, :primary => true
