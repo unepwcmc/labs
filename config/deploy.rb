5debcb547b39ad9a2dea0e8f8a566a0ec73a43c6
Simao Belchior de Castro <simaobelchior@gmail.com>
2012-08-06 17:37:43 +0100

    Trying to make whenever work

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,7 @@
 set :default_stage, 'staging'
 require 'capistrano/ext/multistage'
+
+set :whenever_command, "bundle exec whenever"
 require "whenever/capistrano"
 
 ## Generated with 'brightbox' on Thu Apr 21 11:12:49 +0100 2011
