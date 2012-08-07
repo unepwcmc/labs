5022d752f64f682f1be4841cde4b379d0fd8e98b
James Cox <th3james@gmail.com>
2012-08-07 17:25:36 +0100

    Speed up toggl task behavior

--- app/models/project.rb
+++ app/models/project.rb
@@ -88,39 +88,49 @@ class Project < ActiveRecord::Base
   def self.update_toggl_percentages
     project_stats = {}
 
+    top_3_projects = Project.top_3
+    top_3_toggl_ids = top_3_projects.map(&:toggl_id)
+
     # Get hours worked in projects from each user
     TOGGL_API_KEYS.each do |key|
       toggl_interface = Toggl.new(key, "api_token")
 
       # Get time spent
-      toggl_interface.time_entries({"start_date" => Date.parse('1970-1-1'), "end_date" => Date.today}).each do |time_entry|
+      toggl_interface.time_entries({"start_date" => Date.parse('1970-1-1'), "end_date" => Date.today + 1}).each do |time_entry|
         project_id = time_entry['project'].try(:[],'id')
-        # Init project counter if not already inserted
-        project_stats[project_id] = {:spent => 0, :estimated => 0} unless project_stats[project_id].present?
+        if top_3_toggl_ids.include? project_id 
+          # Init project counter if not already inserted
+          project_stats[project_id] = {:spent => 0, :estimated => 0} unless project_stats[project_id].present?
 
-        # Insert duration into project
-        project_stats[project_id][:spent] += time_entry['duration']
+          # Current time entries show up as massive negative numbers, so ignore
+          if time_entry['duration'].to_i > 0
+            # Insert duration into project
+            project_stats[project_id][:spent] += time_entry['duration']
+          end
+        end
       end
 
       # Get time estimates for project
       toggl_interface.projects.each do |project|
         project_id = project['id']
-        # Init project counter if not already inserted
-        project_stats[project_id] = {:spent => 0, :estimated => 0} unless project_stats[project_id].present?
+        if top_3_toggl_ids.include? project_id 
+          # Init project counter if not already inserted
+          project_stats[project_id] = {:spent => 0, :estimated => 0} unless project_stats[project_id].present?
 
-        # Set estimated
-        project_stats[project_id][:estimated] = project['estimated_workhours']*60*60 if project['estimated_workhours'].present?
+          # Set estimated
+          project_stats[project_id][:estimated] = project['estimated_workhours']*60*60 if project['estimated_workhours'].present?
+        end
       end
     end
 
-    Project.top_3.each_with_index do |project, i|
+    top_3_projects.each_with_index do |project, i|
       # get project toggle id
       stats = project_stats[project.toggl_id]
 
       # calculate percentage from project_stats hash
       percentage = 0
       percentage = stats[:spent].to_f/stats[:estimated] if stats[:estimated] > 0
-      puts "#{project.title}: #{stats[:spent].to_f}/#{stats[:estimated]} = #{percentage}"
+      puts "#{project.title} (#{project.toggl_id}): #{stats[:spent].to_f}/#{stats[:estimated]} = #{percentage}"
 
       response = HTTParty.post("https://push.ducksboard.com/v/#{BUDGET_WIDGET_IDS[i]}", :basic_auth => {
           :username => CONFIG['ducksboard_api_token'],
@@ -129,7 +139,6 @@ class Project < ActiveRecord::Base
         :body => "{\"value\": #{percentage}}"
       )
     end
-    puts project_stats
   end
 end
 
