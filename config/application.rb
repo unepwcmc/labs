d1d734a6e9eed4323d5d79365d98adf9dcd0c772
Simao Belchior de Castro <simaobelchior@gmail.com>
2014-09-25 17:20:15 +0100

    Removed attr_accessible, to start using strong parameters. Removed match route, and replaced with get

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,6 @@ module Labs
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    config.active_record.whitelist_attributes = false
 
     # Custom directories with classes and modules you want to be autoloadable.
     # config.autoload_paths += %W(#{config.root}/extras)
