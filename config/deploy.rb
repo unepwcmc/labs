587ad9837ecf5ac716e986a2ba0a36ff5db9b833
Simao Belchior de Castro <simaobelchior@gmail.com>
2014-10-02 10:55:06 +0100

    Updated ruby string version

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@ set :default_stage, 'staging'
 require 'capistrano/ext/multistage'
 
 require 'rvm/capistrano'
-set :rvm_ruby_string, '1.9.3'
+set :rvm_ruby_string, '1.9.3-p484'
 
 set :whenever_command, "bundle exec whenever"
 #require "whenever/capistrano"
