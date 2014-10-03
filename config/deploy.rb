0f41a1b330084595cadce578fc8871be72792fa0
Simao Belchior de Castro <simaobelchior@gmail.com>
2014-10-03 15:21:03 +0100

    Added required libs to capistrano deploy.rb file. And access to slack_token by loading the YAML file, as there's no Rails environment loaded

--- config/deploy.rb
+++ config/deploy.rb
@@ -30,17 +30,16 @@ set :git_enable_submodules, 1
 default_run_options[:pty] = true # Must be set for the password prompt from git to work
 
 # Slack Notifications
-set :slack_token, Rails.application.secrets.slack_token # comes from inbound webhook integration
+require 'capistrano/slack'
+require 'yaml'
+set :secrets, YAML.load(File.open('config/secrets.yml'))
+
+set :slack_token, secrets["slack_token"] # comes from inbound webhook integration
 set :slack_room, "#labs"
 set :slack_subdomain, "wcmc" # if your subdomain is kohactive.slack.com
 set :slack_application, "Labs"
 set :slack_username, "Capistrano"
 set :slack_emoji, ":thumbsup:"
-
-before 'deploy', 'slack:starting'
-after 'deploy',  'slack:finished'
-
-
 
 ## Dependencies
 # Set the commands and gems that your application requires. e.g.
