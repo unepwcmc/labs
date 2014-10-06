5dfab8b75afb008760d3e139f92ab7dd99a0cd67
Spencer Dixon <spencerlloyddixon@gmail.com>
2014-10-06 11:11:36 +0100

    Refined authentication to be restricted to wcmc-core-dev team on github, also cleaned up methods in doing so

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -9,11 +9,15 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
             sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
             set_flash_message(:notice, :success, :kind => "Github") if is_navigational_format?
         else
-            redirect_to root_path, notice: "You need to be a member of the WCMC github to sign in."
+            reject_login
         end
     else
-        session["devise.github_data"] = request.env["omniauth.auth"]
-        redirect_to root_path, notice: "You need to be a member of the WCMC github to sign in."
+        reject_login
     end
+  end
+
+  def reject_login
+    session["devise.github_data"] = request.env["omniauth.auth"]
+    redirect_to root_path, notice: "You need to be a member of the wcmc-core-dev group on github to be authorized to access this page."
   end
 end
