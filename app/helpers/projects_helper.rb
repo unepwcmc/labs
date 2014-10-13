7dcfdc434f12a1b124a5dcd290c44a7aa647e150
Spencer Dixon <spencerlloyddixon@gmail.com>
2014-10-13 13:19:24 +0100

    Added seeds and placeholder image for projects without screenshots

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,2 +1,6 @@
 module ProjectsHelper
+  def project_preview project
+    source = project.screenshot.thumb.url.blank? ? "http://placehold.it/320x200" : project.screenshot.thumb.url
+    image_tag(source)
+  end
 end
