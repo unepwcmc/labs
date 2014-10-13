62ca0e11b4cbc25443fc7e7ee817e87bc155fbc9
Spencer Dixon <spencerlloyddixon@gmail.com>
2014-10-13 13:19:24 +0100

    Added seeds and placeholder image for projects without screenshots

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+3.times do
+  Project.create!(
+    title: Faker::Company.name,
+    description: Faker::Lorem.paragraph,
+    repository_url: Faker::Internet.url,
+    state: ['Under Development', 'Delivered', 'Project Development'].sample,
+    internal_client: Faker::Name.name,
+    current_lead: Faker::Name.name,
+    external_clients: [ Faker::Name.name, Faker::Name.name, Faker::Name.name],
+    project_leads: [ Faker::Name.name, Faker::Name.name, Faker::Name.name],
+    developers: [ Faker::Name.name, Faker::Name.name, Faker::Name.name],
+    published: true
+  )
+end
+
+puts "Added 3 new seed projects!"
