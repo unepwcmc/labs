05c8f31898d33ca1c0a724dbe91d683c62063cd6
Spencer Dixon <spencerlloyddixon@gmail.com>
2014-10-15 14:23:20 +0100

    Fixed devise scope issue

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Labs::Application.routes.draw do
 
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }, :skip => [:registrations, :sessions, :passwords]
-  delete '/users/sign_out', to: 'devise/sessions#destroy', as: 'destroy_user_session'
+  devise_scope :user do
+    delete '/users/sign_out', to: 'devise/sessions#destroy', as: 'destroy_user_session'
+  end
 
   resources :projects
 
