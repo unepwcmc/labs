e899be641e3bd78555ceafac0376bd155567956a
Simao Belchior de Castro <simaobelchior@gmail.com>
2014-10-20 15:34:26 +0100

    Moving javascript from manifest file to home.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,19 +11,3 @@
 //= require bootstrap.min
 //
 
-$(document).ready(function(){
-  $(".project").hover(function(event){
-    $(this).find('a').css('text-decoration', 'underline');
-    $(this).find('img').css('border', '2px solid #33B5E5');
-  }, function(event){
-    $(this).find('a').css('text-decoration', 'none');
-    $(this).find('img').css('border', 'none');
-  });
-
-  $(".tag-field").select2({tags:[]}, {
-  	placeholder: "Select tag",
-    allowClear: true,
-    minimumInputLength: 1,
-    width: '100%'
-  });
-});
