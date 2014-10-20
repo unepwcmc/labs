9048836a1c3850cc0abea815e73cada058bc36ca
Simao Belchior de Castro <simaobelchior@gmail.com>
2014-10-20 15:34:26 +0100

    Moving javascript from manifest file to home.js

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,2 +1,19 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+$(document).ready(function(){
+  $(".project").hover(function(event){
+    $(this).find('a').css('text-decoration', 'underline');
+    $(this).find('img').css('border', '2px solid #33B5E5');
+  }, function(event){
+    $(this).find('a').css('text-decoration', 'none');
+    $(this).find('img').css('border', 'none');
+  });
+
+  $(".tag-field").select2({tags:[]}, {
+  	placeholder: "Select tag",
+    allowClear: true,
+    minimumInputLength: 1,
+    width: '100%'
+  });
+});
