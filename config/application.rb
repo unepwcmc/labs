aa8729e511dacb3fe486c306f4ad029f5d77c185
Simao Belchior de Castro <simaobelchior@gmail.com>
2014-10-27 14:40:21 +0000

    .

--- config/application.rb
+++ config/application.rb
@@ -38,7 +38,8 @@ module Labs
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
-    config.assets.paths << Rails.root.join("vendor", "assets")
+    config.assets.paths << Rails.root.join("vendor", "assets", "images")
+    config.assets.precompile += %w(*.png *.gif)
 
   end
 end
