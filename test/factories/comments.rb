adec03621220c7604a8a0e086ed145e2eaa62e92
Ferdinando Primerano <ferdinando.primerano@unep-wcmc.org>
2014-11-07 18:47:08 +0000

    Updatet test and added foreign keys

--- test/factories/comments.rb
+++ test/factories/comments.rb
@@ -13,29 +13,29 @@
 
 FactoryGirl.define do
 
-  before(:each) do
-    @user = FactoryGirl.build(:user)
+  before(:create) do
   end
 
   factory :comment do
     content Faker::Lorem.name
+    user_id { FactoryGirl.create(:user).id }
   end
 
   factory :project_comment, class: "Comment" do
     content Faker::Lorem.name
-    user_id { FactoryGirl.build(:user).id }
+    user_id { FactoryGirl.create(:user).id }
     association :commentable, factory: :project
   end
 
   factory :installation_comment, class: "Comment" do
     content Faker::Lorem.name
-    user_id { FactoryGirl.build(:user).id }
+    user_id { FactoryGirl.create(:user).id }
     association :commentable, factory: :installation
   end
 
   factory :server_comment, class: "Comment" do
     content Faker::Lorem.name
-    user_id { FactoryGirl.build(:user).id }
+    user_id { FactoryGirl.create(:user).id }
     association :commentable, factory: :server
   end
 end
