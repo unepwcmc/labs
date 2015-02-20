74bfcebb4fb77632b0a7f128c106a45fcdd48428
Ferdinando Primerano <ferdinando.primerano@unep-wcmc.org>
2015-02-20 10:00:41 +0000

    Added jquery-ui-rails with datepicker

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -3,6 +3,8 @@
 
 $(document).ready(function(){
 
+  $(".datepicker").datepicker()
+
   $(".project").hover(function(event){
     $(this).find('a').css('text-decoration', 'underline');
     $(this).find('img').css('border', '2px solid #33B5E5');
