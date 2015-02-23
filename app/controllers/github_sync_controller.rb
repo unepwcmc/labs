3e319e7462302fb706892e15097bec94533d7581
Spencer Dixon <spencerlloyddixon@gmail.com>
2015-02-23 12:46:26 +0000

    Refactored to include authenticated requests, and handle validation errors for multiple projects

--- app/controllers/github_sync_controller.rb
+++ app/controllers/github_sync_controller.rb
@@ -4,7 +4,19 @@ class GithubSyncController < ApplicationController
   end
 
   def sync
-    GithubProjectSynchroniser.create_from_repos params[:repos]
-    redirect_to projects_path, notice: "Your projects have been created from the information in their github repositories!"
+    repos = GithubProjectSynchroniser.create_from_repos params[:repos]
+
+    if contains_invalid_repository? repos
+      @repos = Github.repos
+      @errored_repos = repos.select { |r| r.errors }
+      render :index
+    else
+      redirect_to projects_path, notice: "All projects were created successfully!"
+    end
   end
+
+  private
+    def contains_invalid_repository? repos
+      repos.find { |r| !r.valid? }
+    end
 end
--- app/models/github_project_synchroniser.rb
+++ app/models/github_project_synchroniser.rb
@@ -1,17 +1,18 @@
 class GithubProjectSynchroniser
   # Takes an array of repo names and loops over it, grabbing them from github and creating a project
   # record (if it doesn't already exist), populating as much information as it can from that repository.
+  # Returns an array of the projects to loop through for errors
   def self.create_from_repos repo_names
-    array = []
-    repo_names.each do |name|
-      array << repo = Github.get_repo(name)
+    repo_names.map { |name|
+      repo = Github.get_repo(name)
       Project.where(github_identifier: repo.full_name).first_or_create do |r|
         r.title = repo.name
         r.state = "Under Development"
         r.github_identifier = repo.full_name
         r.description = repo.description
       end
-    end
-    array
+    }.compact
   end
 end
+
+
