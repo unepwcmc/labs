6d89971fcdfddc84169ba9dc63f8cf3a61dddd1b
Ferdinando Primerano <ferdinando.primerano@unep-wcmc.org>
2015-03-09 14:17:06 +0000

    Added formatting assertions

--- test/controllers/comments_controller_test.rb
+++ test/controllers/comments_controller_test.rb
@@ -13,26 +13,34 @@ class CommentsControllerTest < ActionController::TestCase
     sign_in @user
   end
 
+  def assert_formatting comment
+    json = JSON.parse(@response.body)
+    assert_equal "<p>#{comment.content}</p>\n", json['content']
+  end
+
   test "should have content" do
     assert_not_nil(@project_comment.content, "Content should not be nil")
   end
 
   test "should create project_comment" do
     assert_difference("Comment.count") do
-      post :create, comment: {content: @project_comment.content}, user_id: @user, project_id: @project_comment.commentable_id, format: :json
+      @response = post :create, comment: {content: @project_comment.content}, user_id: @user, project_id: @project_comment.commentable_id, format: :json
     end
+    assert_formatting @project_comment
   end
 
   test "should create installation_comment" do
     assert_difference("Comment.count") do
-      post :create, comment: {content: @installation_comment.content}, user_id: @user, installation_id: @installation_comment.commentable_id, format: :json
+      @response = post :create, comment: {content: @installation_comment.content}, user_id: @user, installation_id: @installation_comment.commentable_id, format: :json
     end
+    assert_formatting @installation_comment
   end
 
   test "should create server_comment" do
     assert_difference("Comment.count") do
-      post :create, comment: {content: @server_comment.content}, user_id: @user, server_id: @server_comment.commentable_id, format: :json
+      @response = post :create, comment: {content: @server_comment.content}, user_id: @user, server_id: @server_comment.commentable_id, format: :json
     end
+    assert_formatting @server_comment
   end
 
 end
