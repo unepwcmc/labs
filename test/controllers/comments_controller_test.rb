574f00dd50ce4a774718020f127f8df8cc050660
Spencer Dixon <spencerlloyddixon@gmail.com>
2015-03-18 10:37:43 +0000

    Fixed tests

--- test/controllers/comments_controller_test.rb
+++ test/controllers/comments_controller_test.rb
@@ -19,7 +19,7 @@ class CommentsControllerTest < ActionController::TestCase
   end
 
   def stub_comment
-    stub_request(:post, "https://slack.com").
+    stub_request(:post, "http://slack.com/").
     with(:body => { payload: { channel: "#labs", username: "New Comment in #labs", text: "Faker::Lorem", icon_emoji: ":envelope:"
     }.to_json },
          :headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'User-Agent'=>'Ruby'}).
@@ -31,7 +31,7 @@ class CommentsControllerTest < ActionController::TestCase
   end
 
   test "should create project_comment" do
-    stub_request(:post, "http://slack.com").
+    stub_request(:post, "http://slack.com/").
     with(:body => "payload=%7B%22channel%22%3A%22%23labs%22%2C%22username%22%3A%22New%20Comment%20in%20%23labs%22%2C%22text%22%3A%22Faker%3A%3ALorem%22%2C%22icon_emoji%22%3A%22%3Aenvelope%3A%22%7D",
          :headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'User-Agent'=>'Ruby'}).
     to_return(:status => 200, :body => "", :headers => {})
