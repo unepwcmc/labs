898cc131bf167d102928051886519bb22b8af377
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2015-03-18 12:26:06 +0000

    remoced trailing spaces

--- app/services/slack_channel.rb
+++ app/services/slack_channel.rb
@@ -4,11 +4,11 @@ require 'yaml'
 class SlackChannel
   def self.post channel, message
     url = Rails.application.secrets.slack_notification_webhook_url
- 
+
     payload = {
-      channel: channel, 
-      username: "New Comment in #{channel}", 
-      text: message, 
+      channel: channel,
+      username: "New Comment in #{channel}",
+      text: message,
       icon_emoji: ":envelope:"
     }
 
