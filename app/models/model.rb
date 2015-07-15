086438fcc8c800738a14c8f20a29593a7423fbc6
Ferdinando Primerano <ferdinando.primerano@unep-wcmc.org>
2015-07-15 13:13:28 +0100

    Added method for retrieving all relationships

--- app/models/model.rb
+++ app/models/model.rb
@@ -3,4 +3,8 @@ class Model < ActiveRecord::Base
   has_many :columns, dependent: :destroy
   has_many :left_relationships, class_name: "Relationship", foreign_key: "left_model_id", dependent: :destroy
   has_many :right_relationships, class_name: "Relationship", foreign_key: "right_model_id", dependent: :destroy
+
+  def all_relationships
+    left_relationships + right_relationships
+  end
 end
