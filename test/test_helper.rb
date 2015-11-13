9eb78a2ee8511d7ba99a43b962ac064063d6a47e
Ferdinando Primerano <ferdinando.primerano@unep-wcmc.org>
2015-11-13 16:23:26 +0000

    Fixed mocha error using mini_test instead of test_unit

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -12,7 +12,7 @@ ENV["RAILS_ENV"] = "test"
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 require 'capybara/rails'
-require 'mocha/test_unit'
+require 'mocha/mini_test'
 require 'database_cleaner'
 require 'webmock/minitest'
 
