97317796683c5ad73ad6a17fcf7d67e81a2a5894
Ferdinando Primerano <ferdinando.primerano@unep-wcmc.org>
2016-03-03 11:33:52 +0000

    API authentication with token

--- app/controllers/api/projects_domains_controller.rb
+++ app/controllers/api/projects_domains_controller.rb
@@ -1,9 +1,21 @@
 class Api::ProjectsDomainsController < ApplicationController
+  before_action :authenticate
+
   def upload_model
     json = JSON.parse(request.body.read)
 
     Domain.add_domain(json)
 
     render json: json
+  end
+
+  private
+
+  def authenticate
+    token = request.headers['X-Authentication-Token']
+    unless token == ENV['API_TOKEN']
+      head status: :unauthorized
+      return false
+    end
   end
 end
