90a12a1f91ec2df415ecf076c55fb659d7d62213
Ferdinando Primerano <ferdinando.primerano@unep-wcmc.org>
2016-07-01 15:48:35 +0100

    Extract common funcionality for sending slack notification when closing

--- app/services/slack_channel.rb
+++ app/services/slack_channel.rb
@@ -2,6 +2,19 @@ require 'httparty'
 require 'yaml'
 
 class SlackChannel
+
+  def self.closing_notification(obj, _params)
+    closing_flag = obj.closing
+    if obj.update_attributes(_params)
+      if closing_flag != obj.closing
+        status = obj.closing ? "scheduled for close down" : "unscheduled for close down"
+        message = "*#{obj.name}* #{obj.class.name.downcase} has been #{status}"
+        post("#labs", "Labs detective (#{Rails.env})", message, ":squirrel:")
+      end
+      yield
+    end
+  end
+
   def self.post channel, username, message, icon
     url = Rails.application.secrets.slack_notification_webhook_url
 
