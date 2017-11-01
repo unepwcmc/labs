15198942bd53f881ae4cd4a4d1cd62a920033102
Andrew Potter <hello@drewpotter.com>
2017-11-01 11:32:01 +0000

    Made alterations based on feedback and now storing admin email in constant

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,11 +1,11 @@
 class NotificationMailer < ApplicationMailer
-  @@admin = Rails.application.secrets.notification_email_recipient_address
+  ADMIN = Rails.application.secrets.notification_email_recipient_address
 
   def notify_team_of_new_project_code(project, existing_project_code, current_user)
     @project = project
     @existing_project_code = existing_project_code
     @current_user = current_user
-    mail(to: @@admin, subject: "Labs: #{@project.title} Project Code alteration")
+    mail(to: ADMIN, subject: "Labs: #{@project.title} Project Code alteration")
   end
 
 end
