64ce2109db672cdc48f847c27b6be3cac7ccc6ce
Andrew Potter <hello@drewpotter.com>
2017-11-02 09:55:58 +0000

    Added test for notification mailer

--- test/mailers/notification_mailer_test.rb
+++ test/mailers/notification_mailer_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class NotificationMailerTest < ActionMailer::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "email notification" do
+    user = FactoryGirl.create(:user)
+    project = FactoryGirl.create(:project, title: "Example project title")
+    email = NotificationMailer.notify_team_of_new_project_code(project, '3.14159265', user)
+
+    assert_emails 1 do
+      email.deliver_now
+    end
+
+    assert_equal ['no-reply@unep-wcmc.org'], email.from
+    assert_equal ['informatics@unep-wcmc.org'], email.to
+    assert_equal "Labs: #{project.title} Project Code alteration", email.subject
+
+  end
 end
