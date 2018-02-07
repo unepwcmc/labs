ab03f7e4dccc737bb40f7929dd14e472ee05f219
Stuart Watson <stuart.watson@unep-wcmc.org>
2018-02-07 11:05:04 +0000

    Added CI integration

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,10 @@
 # config valid only for current version of Capistrano
-lock '3.8.2'
+lock '3.4.0'
 
 set :application, 'labs'
 set :repo_url, 'git@github.com:unepwcmc/labs.git'
+
+set :ssh_options, keys: ["config/deploy_id_rsa"] if File.exist?("config/deploy_id_rsa")
 
 set :branch, 'master'
 
@@ -21,7 +23,7 @@ set :ssh_options, {
   forward_agent: true,
 }
 
-set :linked_files, %w{config/database.yml config/config.yml config/secrets.yml .env}
+set :linked_files, %w{config/database.yml config/config.yml config/secrets.yml}
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
@@ -30,11 +32,15 @@ set :keep_releases, 5
 set :passenger_restart_with_touch, false
 
 
+require 'yaml'
+require 'json'
+secrets =  YAML.load(File.open('config/secrets.yml'))
 
-# notify to slack
-set :slackistrano, {
-  channel: ENV.fetch('SLACK_CHANNEL'),
-  klass: Slackistrano::CustomMessaging,
-  webhook: ENV.fetch('SLACK_WEBHOOK_URL')
-}
+set :slack_token, secrets["slack_token"] # comes from inbound webhook integration
+set :slack_room, "#labs"
+set :slack_subdomain, "wcmc" # if your subdomain is kohactive.slack.com
+set :slack_application, "Labs"
+set :slack_username, "Capistrano"
+set :slack_emoji, ":thumbsup:"
+
 
