d3df080cf067b816d2f838a0279f6709aacb2c29
Stanley Liu <sliu1993@hotmail.co.uk>
2020-12-09 17:56:57 +0000

    made further fixes to the snyk importer regex only registering a match if 0 vulnerabilities are present

--- lib/modules/kpi/ci_importer.rb
+++ lib/modules/kpi/ci_importer.rb
@@ -2,13 +2,13 @@
 
 module Kpi::CiImporter
   # TODO - Not sure how to query Travis
-  def exchange_token
+  def self.exchange_token
     endpoint = 'https://api.travis-ci.org/auth/github'
     response = HTTParty.post(
       endpoint,
+      basic_auth: Github::CLIENT_CREDENTIALS,
       query: {
-        'accept': 'application/vnd.travis-ci.2.1+json',
-        'github_token': Rails.application.secrets.github_secret
+        'accept': 'application/vnd.travis-ci.2.1+json'
       },
       headers: { 'User-Agent': 'Labs' }
     )
