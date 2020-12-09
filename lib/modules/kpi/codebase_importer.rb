36942add236df690a160d04eab67a4b4df9d578f
Stanley Liu <sliu1993@hotmail.co.uk>
2020-12-09 13:08:28 +0000

    bugs backlog size now takes into account subsequent pages of tickets

--- lib/modules/kpi/codebase_importer.rb
+++ lib/modules/kpi/codebase_importer.rb
@@ -9,19 +9,31 @@ module Kpi::CodebaseImporter
   CODEBASE_ENDPOINT = 'https://api3.codebasehq.com'.freeze
 
   def self.bugs_backlog_size
-    url = CODEBASE_ENDPOINT + '/bits-and-bugs/tickets'
-    response = HTTParty.get(
-      url,
+    ticket_count = 0
+    page = 1
+    response = codebase_query(page)
+    
+    until response.response.code == '404'
+      response = codebase_query(page)
+      page += 1
+      tickets = Hash.from_xml(response.body)['tickets']
+      ticket_count += tickets.select do |ticket| 
+        ticket['priority']['name'] == ('High' || 'Critical') && ticket['status']['name'] != 'Completed'
+      end.count
+    end
+
+    ticket_count
+  end
+
+  def self.codebase_query(page)
+    HTTParty.get(
+      CODEBASE_ENDPOINT + '/bits-and-bugs/tickets',
       basic_auth: CODEBASE_CREDENTIALS,
+      query: { 'page': page },
       headers: {
         'Accept': 'application/xml',
         'Content-type': 'application/xml'
       } 
     )
-    tickets = Hash.from_xml(response.body)['tickets']
-
-    tickets.select do |ticket| 
-      ticket['priority']['name'] == ('High' || 'Critical') && ticket['status']['name'] != 'Completed'
-    end.count
   end
 end
