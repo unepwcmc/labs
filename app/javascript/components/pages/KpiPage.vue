961a874e179ff5ce8dee9e96dd9b581ed9203952
Stanley Liu <sliu1993@hotmail.co.uk>
2020-12-11 12:04:57 +0000

    added loading spinner for fetching of kpi data if not instant (or present)

--- app/javascript/components/pages/KpiPage.vue
+++ app/javascript/components/pages/KpiPage.vue
@@ -1,53 +1,57 @@
 <template>
   <div class="page--kpi">
-    <div class="page--kpi__vulnerabilities">
-      <template v-if="kpiStats">
-        <h2>Project vulnerabilities</h2>
-        <div class="page--kpi__chart-row">
-          <chart-doughnut
-            :statistics="kpiStats.project_vulnerability_counts"
-            :title="'Number of public repos sorted by vulnerability count'"
-            :chart-id="'vuln-counts'"
-          ></chart-doughnut>
-        </div>
-        <h2>Project completeness</h2>
-        <div class="page--kpi__chart-row">
-          <chart-doughnut
-            :statistics="kpiStats.percentage_currently_active_products"
-            :title="'% Currently active products'"
-            :chart-id="'active-products'"
-          ></chart-doughnut>
-          <chart-doughnut
-            :statistics="kpiStats.percentage_projects_with_kpis"
-            :title="'% Projects with KPIs'"
-            :chart-id="'kpi-percentages'"
-          ></chart-doughnut>
-        </div>
-        <div class="page--kpi__chart-row">
-          <chart-doughnut
-            :statistics="kpiStats.percentage_projects_documented"
-            :title="'% Projects with documentation'"
-            :chart-id="'documentation-percentages'"
-          ></chart-doughnut>
-          <chart-doughnut
-            :statistics="kpiStats.percentage_projects_with_ci"
-            :title="'% Projects with CI/CD'"
-            :chart-id="'ci-percentages'"
-          ></chart-doughnut>
-        </div>
-        <h2>Bits n Bugs backlog</h2>
-        <h4>Backlog size: {{ kpiStats.bugs_backlog_size }}</h4>
-        <div class="page--kpi__chart-row">
-          <chart-doughnut
-            :statistics="kpiStats.bugs_severity"
-            :title="'Bits N Bugs open tickets sorted by severity'"
-            :chart-id="'bugs-distribution'"
-          ></chart-doughnut>
-        </div>
-        <h2>Total income</h2>
-        <h4>{{ kpiStats.total_income | convert_to_gbp }}</h4>
-      </template>
-    </div>
+    <template v-if="kpiStats">
+      <h2>Project vulnerabilities</h2>
+      <div class="page--kpi__chart-row">
+        <chart-doughnut
+          :statistics="kpiStats.project_vulnerability_counts"
+          :title="'Number of public repos sorted by vulnerability count'"
+          :chart-id="'vuln-counts'"
+        ></chart-doughnut>
+      </div>
+      <h2>Project completeness</h2>
+      <div class="page--kpi__chart-row">
+        <chart-doughnut
+          :statistics="kpiStats.percentage_currently_active_products"
+          :title="'% Currently active products'"
+          :chart-id="'active-products'"
+        ></chart-doughnut>
+        <chart-doughnut
+          :statistics="kpiStats.percentage_projects_with_kpis"
+          :title="'% Projects with KPIs'"
+          :chart-id="'kpi-percentages'"
+        ></chart-doughnut>
+      </div>
+      <div class="page--kpi__chart-row">
+        <chart-doughnut
+          :statistics="kpiStats.percentage_projects_documented"
+          :title="'% Projects with documentation'"
+          :chart-id="'documentation-percentages'"
+        ></chart-doughnut>
+        <chart-doughnut
+          :statistics="kpiStats.percentage_projects_with_ci"
+          :title="'% Projects with CI/CD'"
+          :chart-id="'ci-percentages'"
+        ></chart-doughnut>
+      </div>
+      <h2>Bits n Bugs backlog</h2>
+      <h4>Backlog size: {{ kpiStats.bugs_backlog_size }}</h4>
+      <div class="page--kpi__chart-row">
+        <chart-doughnut
+          :statistics="kpiStats.bugs_severity"
+          :title="'Bits N Bugs open tickets sorted by severity'"
+          :chart-id="'bugs-distribution'"
+        ></chart-doughnut>
+      </div>
+      <h2>Total income</h2>
+      <h4>{{ kpiStats.total_income | convert_to_gbp }}</h4>
+    </template>
+
+    <template v-else>
+      <div class="page--kpi__overlay">
+        <i class="icon--loading-spinner"></i>
+      </div>
+    </template>
   </div>
 </template>
 
