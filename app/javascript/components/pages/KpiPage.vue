69e5aae1a6cd9e6fe707309ee7c5a9395916f0d9
Stanley Liu <sliu1993@hotmail.co.uk>
2020-12-11 15:38:10 +0000

    made non-api-dependent kpi information dynamically available to endpoint

--- app/javascript/components/pages/KpiPage.vue
+++ app/javascript/components/pages/KpiPage.vue
@@ -12,19 +12,19 @@
       <h2>Project completeness</h2>
       <div class="page--kpi__chart-row">
         <chart-doughnut
-          :statistics="kpiStats.percentage_currently_active_products"
+          :statistics="kpiStats.active_products"
           :title="'% Currently active products'"
           :chart-id="'active-products'"
         />
         <chart-doughnut
-          :statistics="kpiStats.percentage_projects_with_kpis"
+          :statistics="kpiStats.feasibility"
           :title="'% Projects with KPIs'"
           :chart-id="'kpi-percentages'"
         />
       </div>
       <div class="page--kpi__chart-row">
         <chart-doughnut
-          :statistics="kpiStats.percentage_projects_documented"
+          :statistics="kpiStats.documentation"
           :title="'% Projects with documentation'"
           :chart-id="'documentation-percentages'"
         />
@@ -48,7 +48,7 @@
       <h2>Yearly updates</h2>
       <div class="page--kpi__chart-row">
         <chart-doughnut
-          :statistics="kpiStats.manual_yearly_updates_overview"
+          :statistics="kpiStats.manual_updates"
           :title="'Overview of manual yearly updates for our projects'"
           :chart-id="'manual-updates'"
         />
