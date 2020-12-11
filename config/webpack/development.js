1cc5418d038d440fbd65578077089d344ebb1b24
Will Kocur <will.kocur@unep-wcmc.org>
2020-12-11 14:29:22 +0000

    add package.json and eslint

---
+++ config/webpack/development.js
@@ -0,0 +1,8 @@
+process.env.NODE_ENV = process.env.NODE_ENV || 'development'
+
+// const eslint = require('./loaders/eslint')
+const environment = require('./environment')
+
+// environment.loaders.prepend('eslint', eslint)
+
+module.exports = environment.toWebpackConfig()
