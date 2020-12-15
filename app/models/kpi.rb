e39f39d398bd719c06835362e6115fbdcf8edc37
Stanley Liu <sliu1993@hotmail.co.uk>
2020-12-15 18:37:22 +0000

    refactored a few methods in kpi model

--- app/models/kpi.rb
+++ app/models/kpi.rb
@@ -82,67 +82,61 @@ class Kpi < ApplicationRecord
   def self.projects_with_ci
     projects_with_ci = Kpi::CiImporter.find_projects_with_ci
 
-    {
-      ci_present: convert_to_percentage(projects_with_ci),
-      ci_absent: convert_to_percentage(Project.count - projects_with_ci)
-    }
+    convert_to_percentage({
+      ci_present: projects_with_ci,
+      ci_absent: Project.count - projects_with_ci
+    })
   end
 
   def self.currently_active_products
     active_projects = Project.where(state: ACTIVE_STATUSES).where.not(last_commit_date: nil).count
 
-    {
-      active_products: convert_to_percentage(active_projects),
-      inactive_products: convert_to_percentage(Project.count - active_projects)
-    }
+    convert_to_percentage({
+      active_products: active_projects,
+      inactive_products: Project.count - active_projects
+    })
   end
 
   def self.projects_with_kpis
     feasible_kpis = Project.where.not(is_feasible: false, key_performance_indicator: nil).count
     unfeasible_kpis = Project.where.not(is_feasible: true, key_performance_indicator: nil).count
 
-    {
-      feasible_kpis: convert_to_percentage(feasible_kpis),
-      unfeasible_kpis: convert_to_percentage(unfeasible_kpis),
-      no_kpis_present: convert_to_percentage(Project.count - (feasible_kpis + unfeasible_kpis))
-    }
+    convert_to_percentage({
+      feasible_kpis: feasible_kpis,
+      unfeasible_kpis: unfeasible_kpis,
+      no_kpis_present: Project.count - (feasible_kpis + unfeasible_kpis)
+    })
   end
 
   def self.projects_with_documentation
     adequate_documentation = Project.where.not(is_documentation_adequate: false, documentation_link: nil).count
     inadequate_documentation = Project.where.not(is_documentation_adequate: true, documentation_link: nil).count
 
-    {
-      adequate_documentation: convert_to_percentage(adequate_documentation),
-      inadequate_documentation: convert_to_percentage(inadequate_documentation),
-      without_documentation: convert_to_percentage(Project.count - (adequate_documentation + inadequate_documentation))
-    }
+    convert_to_percentage({
+      adequate_documentation: adequate_documentation,
+      inadequate_documentation: inadequate_documentation,
+      without_documentation: Project.count - (adequate_documentation + inadequate_documentation)
+    })
   end
 
   def self.manual_yearly_updates_overview
-    none = 0
-    zero_to_ten = 0
-    more_than_ten = 0
+    hash = Hash.new(0)
 
     Project.find_each do |project|
       if project.manual_yearly_updates.zero?
-        none += 1 
+        hash[:none] += 1 
       elsif project.manual_yearly_updates.positive? && project.manual_yearly_updates < 10
-        zero_to_ten += 1
+        hash['0 to 10'] += 1
       else
-        more_than_ten += 1
+        hash['10 to 20'] += 1
       end
     end
 
-    {
-      none: none,
-      "0 to 10": zero_to_ten,
-      "10 to 20": more_than_ten
-    }
+    hash
   end
 
-  def self.convert_to_percentage(count)
-    ((count.to_f / Project.count) * 100).round(2)
+  def self.convert_to_percentage(hash)
+    hash.each { |_key, value| ((value.to_f / Project.count) * 100).round(2) }
   end
 
   private_class_method :currently_active_products, :projects_with_kpis, :projects_with_ci,
