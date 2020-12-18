d93bc26c4ff9367adfb81836d08ac1d7955c8e79
Stanley Liu <sliu1993@hotmail.co.uk>
2020-12-18 10:00:23 +0000

    added required dimension, metric and date range for report request

--- app/services/google_analytics.rb
+++ app/services/google_analytics.rb
@@ -4,16 +4,44 @@ require 'google/apis/analyticsreporting_v4'
 require 'googleauth'
 
 class GoogleAnalytics
-  ANALYTICS = Google::Api::AnalyticsreportingV4
+  ANALYTICS = Google::Apis::AnalyticsreportingV4
+
+  # Default number of times which Google will retry calling the API
+  Google::Apis::RequestOptions.default.retries = 3
 
   def initialize
     @analytics = ANALYTICS::AnalyticsReportingService.new
     # Authorise with our read-only credentials
     @analytics.authorization = google_authorization
+    @date_range = date_range
+    @dimension = dimension
+    @metric = metric
+  end
+
+  def date_range
+    # Retrieves data from the last 90 days
+    Google::Apis::AnalyticsreportingV4::DateRange.new(
+      start_date: 3.months.ago,
+      end_date: DateTime.now
+    )
+  end
+
+  def dimension
+    Google::Apis::AnalyticsreportingV4::Dimension.new(
+      name: "ga:browser"
+    )
+  end
+
+  def metric 
+    Google::Apis::AnalyticsreportingV4::Metric.new(
+      expression: "ga:users"
+    )
   end
 
   def get_user_count
-  
+    Project.find_each do |project|
+
+    end
   end
 
   private
