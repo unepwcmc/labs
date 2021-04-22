09970ae9566b07295aad3f2c2b82a80959f92e63
Will Kocur <will.kocur@unep-wcmc.org>
2021-04-22 10:27:41 +0100

    comment out broken migration code

--- db/migrate/20141105144405_change_data_type_for_internal_clients_in_projects.rb
+++ db/migrate/20141105144405_change_data_type_for_internal_clients_in_projects.rb
@@ -2,12 +2,19 @@ class ChangeDataTypeForInternalClientsInProjects < ActiveRecord::Migration
   def change
     add_column :projects, :internal_clients, :text, array: true, default: []
 
-    Project.reset_column_information
+    # The Project model has now been renamed and therefore this
+    # code is invalid. For development purposes, you can import
+    # a database dump from either production or staging.
 
-    Project.all.each do |p|
-      p.internal_clients << p.try(:internal_client)
-      p.save
-    end
+    # The mapping of internal_client to internal_clients has
+    # already taken place on the staging and production databases. 
+
+    # Project.reset_column_information
+
+    # Project.all.each do |p|
+    #   p.internal_clients << p.try(:internal_client)
+    #   p.save
+    # end
 
     remove_column :projects, :internal_client, :string
   end
