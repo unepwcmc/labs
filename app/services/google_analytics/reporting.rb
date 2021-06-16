38a81f35661148b320c6cc239b6dd9fb85acfd20
Stanley Liu <sliu1993@hotmail.co.uk>
2021-06-16 19:08:38 +0100

    refactor: move authentication into a base class and remove redundant instance variables

--- app/services/google_analytics/reporting.rb
+++ app/services/google_analytics/reporting.rb
@@ -1,26 +1,25 @@
 # frozen_string_literal: true
 
 require 'google/apis/analyticsreporting_v4'
-require 'googleauth'
 
-class GoogleAnalytics
+class GoogleAnalytics::Reporting < GoogleAnalytics::Base
   ANALYTICS = Google::Apis::AnalyticsreportingV4
 
   # Default number of times which Google will retry calling the API
   Google::Apis::RequestOptions.default.retries = 3
 
   # date default to 3 months ago (90 days) for fetching the user count per product
-  def initialize(date = 3.months.ago)
+  def initialize(from_date = 3.months.ago)
     @analytics = ANALYTICS::AnalyticsReportingService.new
+    
     # Authorise with our read-only credentials
     @analytics.authorization = google_authorization
-    @date = date.strftime('%Y-%m-%d')
-    @date_range = date_range
-    @dimension = dimension
-    @metric = metric
+
+    # Google API only accepts dates in 'YYYY-MM-DD' format
+    @from_date = from_date.strftime('%Y-%m-%d')
   end
 
-  def get_user_count(google_tracking_code)
+  def new_request(google_tracking_code)
     request = ANALYTICS::GetReportsRequest.new(
       { report_requests: [new_report_request(google_tracking_code)] }
     )
@@ -30,20 +29,12 @@ class GoogleAnalytics
 
   private
 
-  def google_authorization
-    # Create service account credentials
-    #
-    # You will need a copy of the credentials JSON in the root of your repository
-    Google::Auth::ServiceAccountCredentials.make_creds(
-      json_key_io: File.open('service_account_credentials.json'),
-      scope: 'https://www.googleapis.com/auth/analytics.readonly'
-    )
-  end
-
   def date_range
+    today = Date.today.strftime('%Y-%m-%d')
+
     ANALYTICS::DateRange.new(
-      start_date: @date,
-      end_date: Date.today.strftime('%Y-%m-%d')
+      start_date: @from_date,
+      end_date: today
     )
   end
 
