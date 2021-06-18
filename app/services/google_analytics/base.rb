a71f399db4efbd553529cff0d6a7a92dd4d87bd1
Stanley Liu <sliu1993@hotmail.co.uk>
2021-06-18 17:46:13 +0100

    refactor: use nested modules instead of namespacing

--- app/services/google_analytics/base.rb
+++ app/services/google_analytics/base.rb
@@ -1,14 +1,17 @@
-class GoogleAnalytics::Base 
+# frozen_string_literal: true
 
-  private
+module GoogleAnalytics
+  class Base
+    private
 
-  def google_authorization
-    # Authorise against service account credentials
-    #
-    # You will need a copy of the credentials JSON in the root of your repository
-    Google::Auth::ServiceAccountCredentials.make_creds(
-      json_key_io: File.open('service_account_credentials.json'),
-      scope: 'https://www.googleapis.com/auth/analytics.readonly'
-    )
+    def google_authorization
+      # Authorise against service account credentials
+      #
+      # You will need a copy of the credentials JSON in the root of your repository
+      Google::Auth::ServiceAccountCredentials.make_creds(
+        json_key_io: File.open('service_account_credentials.json'),
+        scope: 'https://www.googleapis.com/auth/analytics.readonly'
+      )
+    end
   end
 end
