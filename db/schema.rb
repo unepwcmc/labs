cc9176c7d5f30143b1a29df0fffbdc41c6d23b53
Stanley Liu <sliu1993@hotmail.co.uk>
2021-06-22 17:04:02 +0100

    fix: reorder migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210617114308) do
+ActiveRecord::Schema.define(version: 20210618202349) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
