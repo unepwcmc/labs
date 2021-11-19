312f245b783a8c151d47c23a31aa609d9eff1784
sergio-marrocoli <sergio@smarteditors.co.uk>
2021-11-19 10:08:05 +0000

    Updated github teams query request (https://developer.github.com/changes/2020-02-10-deprecating-auth-through-query-param/), reverted to using cookies

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -4,14 +4,14 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     @user.set_token(request.env["omniauth.auth"])
 
     if @user.persisted?
-        if @user.is_dev_team?
-            sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
-            set_flash_message(:notice, :success, :kind => "Github") if is_navigational_format?
-        else
-            reject_login
-        end
-    else
+      if @user.is_dev_team?
+        sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
+        set_flash_message(:notice, :success, :kind => "Github") if is_navigational_format?
+      else
         reject_login
+      end
+    else
+      reject_login
     end
   end
 
